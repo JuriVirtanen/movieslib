0aad543b902d36f575b1ced49aac0250e3a972a5
Gabriel Marczak <121840299+JuriVirtanen@users.noreply.github.com>
2023-01-31 16:10:10 +0100

    Finish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+from datetime import date
 
 
 class Movie:
@@ -42,6 +43,11 @@ def generate_views(lista):
     los.add_view(random.randint(1, 100))
 
 
+def magic_function(lista):
+    for ile_razy in range(10):
+        generate_views(lista)
+
+
 def get_movies(lista):
     only_movies = []
     for video in lista:
@@ -64,48 +70,59 @@ def search(lista, search_word): # nie jestem pewny czy o coś takiego chodziło
             print(video)
 
 
+def ile_odcinkow(lista, title, ile=0):
+    for video in lista:
+        if title in str(video):
+            ile += 1
+    return ile
+
+
 def top_titles(lista, topx, content_type="all"):
     if content_type == "all":
         top = sorted(lista, key=lambda video: video.views, reverse=True)
         repeat = 1
+        print(f"Najpopularniejsze filmy i seriale dnia {normalna_data}")
         for rank_video in top:
-            print(rank_video)
+            print(f"{repeat}. {rank_video}")
             if repeat == topx:
                 break
             repeat += 1
     elif content_type == "Movies":
         top = sorted(get_movies(lista), key=lambda video: video.views, reverse=True)
         repeat = 1
+        print(f"Najpopularniejsze filmy i seriale dnia {normalna_data}")
         for rank_video in top:
-            print(rank_video)
+            print(f"{repeat}. {rank_video}")
             if repeat == topx:
                 break
             repeat += 1
     elif content_type == "Series":
         top = sorted(get_series(lista), key=lambda video: video.views, reverse=True)
         repeat = 1
+        print(f"Najpopularniejsze filmy i seriale dnia {normalna_data}")
         for rank_video in top:
-            print(rank_video)
+            print(f"{repeat}. {rank_video}")
             if repeat == topx:
                 break
             repeat += 1
 
 
+def add_season(n_episodes, season, title, year, genre, lista):
+    for nn in range(n_episodes):
+        lista.append(Series(nn + 1, season, title, year, genre))
+    return lista
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     library = []
-    test1 = Series(1, 1, "Love is War", 2019, "Comedy")
-    test2 = Series(2, 1, "Love is War", 2019, "Comedy")
-    test3 = Series(3, 1, "Love is War", 2019, "Comedy")
-    test4 = Series(4, 1, "Love is War", 2019, "Comedy")
-    test5 = Series(5, 1, "Love is War", 2019, "Comedy")
-    library.append(test1)
-    library.append(test2)
-    library.append(test3)
-    library.append(test4)
-    library.append(test5)
-    test2.play()
-    test2.play()
-    test5.play()
-    test1.play()
-    top_titles(library, 2)
+    today = date.today()
+    normalna_data = f"{str(today.day).zfill(2)}.{str(today.month).zfill(2)}.{today.year}"
+    print("Biblioteka filmów")
+    library = add_season(12, 1, "Fairy Tail", 2009, "Fantasy", library)
+    library = add_season(12, 2, "Fairy Tail", 2010, "Fantasy", library)
+    library = add_season(8, 1, "Black Rock Shooter", 2010, "Thriller", library)
+    library.append(Movie("Avatar", 2009, "Sci-Fi"))
+    library.append(Movie("Avatar 2", 2022, "Sci-Fi"))
+    magic_function(library)
+    top_titles(library, 3)
+    
